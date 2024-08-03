e49c3584176a85eb4c72b32aa779aeaefb739de0
Sruthi <sruthivs64@gmail.com>
2024-08-03 16:33:12 +0530

    Added new message form, handle POST req & improved styling

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const indexRouter = require('./routes/index');
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({extended:true}));
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
 
 app.listen(8080);
--- routes/index.js
+++ routes/index.js
@@ -14,12 +14,33 @@ const messages = [
     }
   ];
 
+  //route to display messages
   router.get('/', (req, res)=>{
     res.render('index', { title: "Mini Messageboard", messages });
   });
 
+  //display form
   router.get('/new', (req, res)=>{
-    res.render('form');
+    res.render('form', {title: "New Message"});
+  });
+
+  //handle form actions
+  router.post('/new', (req, res)=>{
+    const {user, text} = req.body;
+    messages.push({text, user, added: new Date()});
+    res.redirect('/');
   });
   
+  //display message details
+
+  router.get('/message/:id',(req,res)=>{
+    const message = messages[req.params.id];
+    if(message){
+      res.render('message', {title:"Message Details", message});
+    }else{
+      res.status(404).send('Message not found');
+    }
+  });
+
+
   module.exports = router;
